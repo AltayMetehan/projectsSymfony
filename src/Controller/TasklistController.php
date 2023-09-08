142b26780ddd077eadff9191f0cbd8a148fc7af8
Metehan Altay <altaymetehan03@hotmail.com>
2023-09-08 09:06:34 +0000

    validating some errors

--- src/Controller/TasklistController.php
+++ src/Controller/TasklistController.php
@@ -7,18 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-/**
- * @Route("/tasklist", name="task_list")
- */
 class TasklistController extends AbstractController
 {
     #[Route('/tasklist', name: 'task_list')]
     public function list(): Response
     {
-        $tasks = $this->getDoctrine()->getRepository(Task::class)->findAll();
+        /**$tasks = $this->getDoctrine()->getRepository(Task::class)->findAll();
 
         return $this->render('task/list.html.twig', [
             'tasks' => $tasks,
-        ]);
+        ]);*/
+    }
+
+    #[Route('/tasklist/create', name: 'task_create')]
+    public function create(): Response
+    {
+        
+    }
+
+    #[Route('/tasklist/delete', name: 'task_delete')]
+    public function delete(): Response
+    {
+        
+    }
+
+    #[Route('/tasklist/edit', name: 'task_edit')]
+    public function edit(): Response
+    {
+        
     }
 }
