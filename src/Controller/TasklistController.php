2e3b312a514ca059546a2e680ae27978be905a5a
Metehan Altay <altaymetehan03@hotmail.com>
2023-09-08 12:10:28 +0000

    zzz

--- src/Controller/TasklistController.php
+++ src/Controller/TasklistController.php
@@ -6,23 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Tasklists;
-use Doctrine\Persistence\ManagerRegistry;
-use App\Form\TasklistsType;
-use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class TasklistController extends AbstractController
 {
-    private $doctrine;
-    public function __construct(ManagerRegistry $doctrine)
-    {
-        $this->doctrine = $doctrine;
-    }
-
     #[Route('/tasklist', name: 'task_list')]
-    public function list(): Response
+    public function list(EntityManagerInterface $entityManager): Response
     {
-        $tasklists = $this->doctrine->getRepository(Tasklists::class)->findAll();
+        $repository = $entityManager->getRepository(Tasklists::class);
+        $tasklists = $repository->findAll();
 
         return $this->render('tasklist/list.html.twig', [
             'tasklists' => $tasklists,
@@ -30,34 +23,19 @@ class TasklistController extends AbstractController
     }
 
     #[Route('/tasklist/create', name: 'task_create')]
-    public function create(Request $request): Response
-    {
-        $tasklists = new Tasklists();
-
-        $form = $this->createForm(TasklistsType::class, $tasklists);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->doctrine->getManager();
-            $entityManager->persist($tasklists);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('task_list');
-        }
-
-        return $this->render('tasklist/create.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/tasklist/delete/{id}', name: 'task_delete')]
-    public function delete(int $id): Response
+    public function create(): Response
     {
         
     }
 
-    #[Route('/tasklist/edit/{id}', name: 'task_edit')]
-    public function edit(int $id): Response
+    #[Route('/tasklist/delete', name: 'task_delete')]
+    public function delete(): Response
+    {
+        
+    }
+
+    #[Route('/tasklist/edit', name: 'task_edit')]
+    public function edit(): Response
     {
         
     }
